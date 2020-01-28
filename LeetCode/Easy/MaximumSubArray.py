30a5431ccd068f6195d856075cce1b1eceb69d0e
Adriene Tien <attien@edu.uwaterloo.ca>
2020-01-28 08:16:46 -0500

    Max subarray sum, start of max product subarray

--- LeetCode/Easy/MaximumSubArray.py
+++ LeetCode/Easy/MaximumSubArray.py
@@ -1,8 +1,10 @@
 # LeetCode Easy
 # Maximum Sub-array Question
 
+
 class Solution:
 
+    # My own successful attempt at solving
     # O(n) time complexity solution, avoiding double for loop.
     # O(1) space complexity, only need two variables -> constant.
     def maxSubArray(self, nums: List[int]) -> int:
@@ -23,4 +25,25 @@ class Solution:
 
         return maxsum
 
-    # try divide and conquer method
+    # Divide and Conquer Method: Kadane's Algorithm, Dynamic Programming Approach
+    # solution is certainly a bit cleaner than above, and is much faster even though both are O(n) time
+    # O(n) time, O(1) space
+    def maxSubArrayKadane(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        maxsum = nums[0]
+        sum = maxsum
+        for i in range(1, n):
+            # if else block can be simplified to sum = max(sum+nums[i], nums[i])
+            if sum + nums[i] > nums[i]:
+                sum += nums[i]
+            else:
+                sum = nums[i]
+
+            if sum > maxsum:
+                maxsum = sum
+
+        return maxsum
+
+
---
+++ LeetCode/Medium/MaximumProductSubarray.py
@@ -0,0 +1,9 @@
+# LeetCode Medium
+# Maximum Product Array Question
+# Negative multiplication -> positive number, changing the question quite significantly from MaximumSubArray (LC Easy)
+
+class Solution:
+
+    def maxProduct(self, nums: List[int]) -> int:
+        pass
+
