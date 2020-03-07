ef0df8d9e2b3d15092579310c8fc932acca6cc35
Adriene Tien <adrienetien@Adrienes-MacBook-Air.local>
2020-03-07 14:13:22 -0500

    adding 3 meds

--- LeetCode/Medium/KthLargestElementInArray.py
+++ LeetCode/Medium/KthLargestElementInArray.py
@@ -1,6 +1,9 @@
 # LeetCode Medium
 # Kth Largest Element in Array Question
 # Searching for some kth max or kth min automatically suggests to me that this is a heap question
+# Time complexity: our key operations here are the heapify operation and the pop of the maximum element.
+# Pop: O(log(n)), Heapify is O(n). O(n + log(n)) -> O(n) overall time complexity
+# Space complexity: O(1)
 
 import heapq
 
