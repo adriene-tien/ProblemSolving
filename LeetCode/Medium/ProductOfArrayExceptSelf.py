11a3f80819f488ea1aeb5abb45ff3fe8206fef6e
Adriene Tien <17tiena@gmail.com>
2023-06-16 14:40:20 -0400

    product of array self revisited 2

--- LeetCode/Medium/ProductOfArrayExceptSelf.py
+++ LeetCode/Medium/ProductOfArrayExceptSelf.py
@@ -6,11 +6,9 @@
 class Solution:
 
     # O(n) time
-    # O(n) space since I use a left array, could simplify and use only the output array to make it O(1) space since 
-    # sometimes we don't count the return array as extra space
+    # O(1) space since they don't count return array as extra space
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        left = [None]*n
         ret = [None]*n
 
         # focus on populating the left and right product arrays first 
@@ -18,17 +16,15 @@ class Solution:
         left_product = 1
         for i in range(n): 
             if i == 0: 
-                left[i] = 1
+                ret[i] = 1
             else: 
                 left_product *= nums[i-1]
-                left[i] = left_product
+                ret[i] = left_product
         
         right_product = 1
         for i in reversed(range(n)): 
-            if i == n-1:
-                ret[i] = left[i]
-            else: 
+            if i != n-1:
                 right_product *= nums[i+1]
-                ret[i] = left[i]*right_product
+                ret[i] *= right_product
         
         return ret
