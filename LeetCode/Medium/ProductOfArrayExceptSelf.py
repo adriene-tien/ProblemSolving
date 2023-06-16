f1f8b9e9e15b431c5b29c1c5612bbc9e6e13470a
Adriene Tien <17tiena@gmail.com>
2023-06-16 14:37:39 -0400

    product except self revisited

--- LeetCode/Medium/ProductOfArrayExceptSelf.py
+++ LeetCode/Medium/ProductOfArrayExceptSelf.py
@@ -5,93 +5,30 @@
 
 class Solution:
 
-    # try with division first and see where that brings me
-    # as expected, this doesn't get accepted as a solution by LC due to the use of the division here...
-    # but this solution would be O(n) time complexity and O(n) space complexity as len(output) = len(nums)
-    def productExceptSelfWithDivision(self, nums: List[int]) -> List[int]:
+    # O(n) time
+    # O(n) space since I use a left array, could simplify and use only the output array to make it O(1) space since 
+    # sometimes we don't count the return array as extra space
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        total_prod = 1
-        output = []
-        for i in range(0, n):
-            total_prod *= nums[i]
-        for i in range(0, n):
-            output.append(total_prod//nums[i]) # double slash to make sure list is of integers
-        return output
+        left = [None]*n
+        ret = [None]*n
 
-    # now try removing the division approach
-    # Look up and try a bitwise approach, feels like the only way it can be done
-    # time limit still exceeded doing it this way because of while inside for :(
-    def productExceptSelfBitwise(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        total_prod = 1
-        output = []
-        for i in range(0, n):
-            total_prod *= nums[i]
-
-        # total_prod // nums[i]
-        for i in range(0, n):
-
-            sign = 1
-            if nums[i] * total_prod < 0:
-                sign = -1
-
-            mask = 1
-            quotient = 0
-            local_total_prod = total_prod
-            while nums[i] <= local_total_prod:
-                nums[i] <<= 1
-                mask <<= 1
-            while mask > 1:
-                nums[i] >>= 1
-                mask >>= 1
-                if local_total_prod >= nums[i]:
-                    local_total_prod -= nums[i]
-                    quotient |= mask
-            output.append(sign * quotient)
-
-        return output
-
-    # Visualization: Left and Right Sides Product Arrays
-    # Although this completes it in O(n), it is still very slow since we need 3 for loops total.
-    # O(n) time complexity, O(n) space complexity since it is n + n excluding the output array, 2n -> O(n)
-    def productExceptSelfLR(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        left = [None] * n
-        right = [None] * n
-        output = []
-
-        for i in range(0, n):
-            if i == 0:
+        # focus on populating the left and right product arrays first 
+        # left array first
+        left_product = 1
+        for i in range(n): 
+            if i == 0: 
                 left[i] = 1
-            else:
-                left[i] = left[i - 1] * nums[i - 1]
-
-        for i in range(n - 1, -1, -1):
-            if i == n - 1:
-                right[i] = 1
-            else:
-                right[i] = right[i + 1] * nums[i + 1]
-
-        for i in range(0, n):
-            output.append(left[i] * right[i])
-
-        return output
-
-    # Try and reduce the for loops needed
-    # Operate on one array instead of two (the output array itself), while removing the need for 3 loops
-    def productExceptSelf2For(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        output = []
-
-        # initially fills product with what would have been left[] array
-        product = 1
-        for i in range(0, n):
-            output.append(product)
-            product = product * nums[i]
-
-        product = 1
-        for i in range(n-1, -1, -1):
-            output[i] = output[i] * product
-            product = product * nums[i]
-
-        return output
+            else: 
+                left_product *= nums[i-1]
+                left[i] = left_product
+        
+        right_product = 1
+        for i in reversed(range(n)): 
+            if i == n-1:
+                ret[i] = left[i]
+            else: 
+                right_product *= nums[i+1]
+                ret[i] = left[i]*right_product
+        
+        return ret
